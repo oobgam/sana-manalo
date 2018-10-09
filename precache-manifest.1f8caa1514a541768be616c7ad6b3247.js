d9b749507086a9cef1863bc1e23e9b477614d61b
RD Magboo <rdmagboo@gmail.com>
2018-10-09 18:48:57 +0800

    Updates

---
+++ precache-manifest.1f8caa1514a541768be616c7ad6b3247.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d95dea2e932e8b13aeea35fc573981",
+    "url": "/static/media/ULTRA_LOTTO.14d95dea.png"
+  },
+  {
+    "revision": "760f291626344245990ba24289a05e95",
+    "url": "/static/media/SUPER_LOTTO.760f2916.png"
+  },
+  {
+    "revision": "8f432586f5b408622aabce300c759d6e",
+    "url": "/static/media/MEGA_LOTTO.8f432586.png"
+  },
+  {
+    "revision": "baa8b7c6d256b01bd85cec6d970a75ad",
+    "url": "/static/media/LOTTO.baa8b7c6.png"
+  },
+  {
+    "revision": "85efbe71e2b32ff6b9aea852bdea9eaa",
+    "url": "/static/media/GRAND_LOTTO.85efbe71.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c32ad93476288867226d",
+    "url": "/static/js/main.c32ad934.chunk.js"
+  },
+  {
+    "revision": "b64fdaf3f7dfc28b7237",
+    "url": "/static/js/1.b64fdaf3.chunk.js"
+  },
+  {
+    "revision": "00bda7763d3eb6e385f7014e8f91bac8",
+    "url": "/index.html"
+  }
+];
